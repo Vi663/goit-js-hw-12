d999958fbcee75b80bb10a9a8f10b582e6730cc6
Вікторія Юрченко <viktoriau26@gmail.com>
2021-07-20 18:16:27 +0300

    css, markup

--- src/index.js
+++ src/index.js
@@ -1,31 +1,55 @@
 import './css/styles.css';
 import Country from './partials/fetchCountries.js';
 import _ from 'lodash';
+import Notiflix from "notiflix";
 import countryListTemplate from './partials/templates/countryList.hbs'
 import countryMarkupTemplate from './partials/templates/countryMarkup.hbs'
 
-const refs = {
-  input: document.getElementById('search-box'),
-  countryContainer: document.querySelector('.country-list'),
-  infoContainer: document.querySelector('.country-info'),
-}
+const input = document.getElementById('search-box');
+const countryContainer = document.querySelector('.country-list');
+// const infoContainer = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
-refs.input.addEventListener('input', _.debounce(onInputChange, DEBOUNCE_DELAY));
+input.addEventListener('input', _.debounce(onInputChange, DEBOUNCE_DELAY));
 
 const newCountry = new Country();
 function onInputChange(e) {
   const inputValue = e.target.value;
   // e.currentTarget.elements.query.value
-  newCountry.fetchCountries(inputValue);
-  const data = newCountry.fetchCountries(inputValue);
-  const countryMarkup = countryMarkupTemplate(data);
-  refs.countryContainer.innerHTML = countryMarkup;
-  console.log(countryMarkup);
-  console.log(data);
+  newCountry.fetchCountries(inputValue).then((data) => {
+    if (data.length === 1) {
+      renderMarkupFor1Country(data)
+    } else if (data.length > 1 && data.length <= 10) {
+      renderMarkupFor4Countrys(data)
+    } else if (data.length >= 10) {
+      renderMarkupFor1Country()
+      Notiflix.Notify.info(
+        'Too many matches found. Please enter a more specific name.',
+        {
+          timeout: 3000,
+        },
+      );
+    } else if (data.status === 404) {
+      renderMarkupFor1Country()
+      Notiflix.Notify.failure(
+        'Oops, there is no country with that name',
+        {
+          timeout: 3000,
+        },
+      );
+    }
+  });
+  // console.log(countryMarkup);
+  
+  
 };
 
-// function renderMarkup() {
-  
-// }
+function renderMarkupFor1Country(country) {
+  const countryMarkup = countryMarkupTemplate(country);
+  countryContainer.innerHTML = countryMarkup;
+};
+function renderMarkupFor4Countrys(country) {
+  const countryMarkup = countryListTemplate(country);
+  countryContainer.innerHTML = countryMarkup;
+};
 
--- src/partials/fetchCountries.js
+++ src/partials/fetchCountries.js
@@ -1,12 +1,10 @@
+
 export default class Country {
-  constructor() {}
+  constructor() { }
   fetchCountries(name) {
-    // fetch('https://restcountries.eu/rest/v2/all?fields=name;capital;population;flag;languages').then
-    return fetch(`https://restcountries.eu/rest/v2/name/${name}`)
-      .then(response => {response.json()})
-      .then(data => {
-        return data;
-      })
-      .catch(error => console.log(error));
+    return fetch(
+      `https://restcountries.eu/rest/v2/name/${name}`
+    ).then(response => response.json())
+  
   }
 }
